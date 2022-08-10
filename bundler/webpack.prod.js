3453985b6e304d006bde2014785719ad599279e4
DMP92 <devin.pfromm.20@gmail.com>
2022-08-10 06:10:25 -0400

    added repo to package.json

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -7,9 +7,38 @@ module.exports = merge(
     {
     mode: 'production',
     devtool: 'inline-source-map',
-    resolve: {
-        fallback: {
-            "os": require.resolve("os-browserify/browser")
-        }
-    }
-});
+    module: {
+        rules: [
+            // CSS
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+
+            // Images
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+
+            // Fonts
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+
+            // CSV 
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+
+            // XML
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+            
+        ],
+    },
+},);
